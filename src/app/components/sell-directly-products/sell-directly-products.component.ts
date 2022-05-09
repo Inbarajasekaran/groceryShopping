a62e5eae3a40751d16ad115761dfe6407a5733e3
Inbarajasekaran <inbarajasekaran0011@gmail.com>
2022-05-09 16:15:09 +0530

    All errors fixed and folder structure changes , material module fixed

--- src/app/components/sell-directly-products/sell-directly-products.component.ts
+++ src/app/components/sell-directly-products/sell-directly-products.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class SellDirectlyProductsComponent implements OnInit {
 
   constructor() { }
-test = [1,2,3,4]
+  
   ngOnInit(): void {
   }
 
